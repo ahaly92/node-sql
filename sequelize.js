a0e7365c6d81281abfddeb2723ace64ffaa6f214
Ahmed Aly <ahmaly0502@gmail.com>
2019-09-24 12:16:40 -0500

    add config files for heroku

--- sequelize.js
+++ sequelize.js
@@ -1,13 +1,21 @@
 import UserModel from './models/user'
 import Sequelize from 'sequelize'
 
-const sequelize = new Sequelize('company', 'root', 'password', {
+let sequelize = null;
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect:  'postgres',
+        protocol: 'postgres',
+    })
+} else {
+    sequelize = new Sequelize('company', 'root', 'password', {
     host: 'localhost',
     dialect: 'mysql',
     define: {
         timestamps: false
-    }
-});
+    }});
+}
 
 const User = UserModel(sequelize, Sequelize);
 
